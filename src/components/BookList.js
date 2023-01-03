fca6fe591cc75aac0f61656757b8828530fbafa2
Pooja Singh <poojasingh1234gkp@gmail.com>
2023-01-04 01:53:52 +0530

    wip

---
+++ src/components/BookList.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+import BooksContext from '../context/books'
+import BookShow from './BookShow'
+
+const BookList = () => {
+  const { books } = useContext(BooksContext);
+    const renderedBooks = books.map((book) => {
+        return (
+          <BookShow key={book.id} book={book} />
+        )
+    })
+  return (
+    <div className='book-list'>{renderedBooks}</div>
+  )
+}
+
+export default BookList
